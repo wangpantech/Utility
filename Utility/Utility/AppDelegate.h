0aca31c320b00ed5f485059afdd098754e58919e
WangPan <WangPan@virus.local>
2016-08-15 15:35:45 +0800

    init

---
+++ Utility/Utility/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Utility
+//
+//  Created by WangPan on 16/8/15.
+//  Copyright © 2016年 WangPan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Utility/Utility/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Utility
+//
+//  Created by WangPan on 16/8/15.
+//  Copyright © 2016年 WangPan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
